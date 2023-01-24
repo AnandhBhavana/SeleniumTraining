09f4e31a879fe5e97008bc9d534a4b73fa7045c0
AnandhBhavana <anandhabhavana05@gmail.com>
2023-01-24 21:58:39 +0530

    Updated registration functionality

---
+++ src/org/tam/TamilNadu.java
@@ -0,0 +1,14 @@
+package org.tam;
+
+public class TamilNadu {
+	private void district() {
+		System.out.println("how many district in tamilnadu");
+
+	}
+	public static void main(String[] args) {
+		//classname object = new classname();
+		TamilNadu d = new TamilNadu();
+		d.district();
+	}
+
+}
